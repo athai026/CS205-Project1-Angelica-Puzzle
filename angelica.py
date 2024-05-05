a5e06efe8e0e7c4331b5815d56c566b48399a6d1
athai026 <athai026@ucr.edu>
2024-05-04 23:01:48 -0700

    move operators. h(n) to be implemented

--- angelica.py
+++ angelica.py
@@ -25,4 +25,72 @@ class NPuzzle:
         self.blank = p1.blank
         self.goal = p1.goal
 
+    def go_right(self, choice):
+        i, j = self.blank
+        self.puzzle[i][j], self.puzzle[i][j + 1] = self.puzzle[i][j + 1], self.puzzle[i][j]
+        self.blank = (i, j + 1)
+        self.g += 1
+
+        if choice == 2:
+            self.h = self.find_misplaced()
+        elif choice == 3:
+            self.h = self.manhattan_distance()
+
+        self.f = self.g + self.h
+
+    def go_left(self, choice):
+        i, j = self.blank
+        self.puzzle[i][j], self.puzzle[i][j - 1] = self.puzzle[i][j - 1], self.puzzle[i][j]
+        self.blank = (i, j - 1)
+        self.g += 1
+
+        if choice == 2:
+            self.h = self.find_misplaced()
+        elif choice == 3:
+            self.h = self.manhattan_distance()
+
+        self.f = self.g + self.h
+
+    def go_up(self, choice):
+        i, j = self.blank
+        self.puzzle[i][j], self.puzzle[i - 1][j] = self.puzzle[i - 1][j], self.puzzle[i][j]
+        self.blank = (i - 1, j)
+        self.g += 1
+
+        if choice == 2:
+            self.h = self.find_misplaced()
+        elif choice == 3:
+            self.h = self.manhattan_distance()
+
+        self.f = self.g + self.h
+
+    def go_down(self, choice):
+        i, j = self.blank
+        self.puzzle[i][j], self.puzzle[i + 1][j] = self.puzzle[i + 1][j], self.puzzle[i][j]
+        self.blank = (i + 1, j)
+        self.g += 1
+
+        if choice == 2:
+            self.h = self.find_misplaced()
+        elif choice == 3:
+            self.h = self.manhattan_distance()
+
+        self.f = self.g + self.h
+
+    def check_right(self):
+        i, j = self.blank
+        return j != self.size - 1
+
+    def check_left(self):
+        i, j = self.blank
+        return j != 0
+
+    def check_up(self):
+        i, j = self.blank
+        return i != 0
+    
+    def check_down(self):
+        i, j = self.blank
+        return i != self.size - 1
+
     
